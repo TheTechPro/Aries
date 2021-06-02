4b71fce06a870f2468f1be5b4a3877ea4c53402a
Arpan Das Gupta <53475919+TheTechPro@users.noreply.github.com>
2021-06-02 12:36:25 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    $(window).on('load', function() {
+        $('.preloader').addClass('complete')
+    });
+
+    $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop();
+        if (scroll>=50) {
+            $('.sticky').addClass("stickyadd");
+        } else {
+            $('.sticky').removeClass("stickyadd");
+        }
+    })
+
+    var typed = new Typed(".element", {
+        strings:["Aries", "a Developer", "an Aninmator", "a Mangaka"],
+        smartBackspace:true,
+        typeSpeed:100,
+        backSpeed:100,
+        loop:true,
+        loopCount:Infinity,
+        
+    })
+
+// progress bars
+var waypoint = new Waypoint({
+    element: document.getElementById('exp-id'),
+    handler: function() {
+        var p = document.querySelectorAll('.progress-bar');
+        p[0].setAttribute("style", "width:100%;transition:1s all");
+        p[1].setAttribute("style", "width:85%;transition:1.5s all");
+        p[2].setAttribute("style", "width:95%;transition:1.7s all");
+        p[3].setAttribute("style", "width:90%;transition:2s all");
+        p[4].setAttribute("style", "width:80%;transition:2.3s all");
+        p[5].setAttribute("style", "width:100%;transition:2.5s all");
+    }, 
+    offset: '90%'
+  });
+
+  var filterizd = $('.filter-container').filterizr({
+      animationDuration : .5
+  });
+
+});
